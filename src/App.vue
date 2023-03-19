1a3aa489a5339779c2b15b4cf54c9de46b4fcd37
maxarvid <max@klimato.se>
2023-03-19 11:42:39 +0100

    :tada: Scaffolds vue project with vuetify

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
+
+const drawer = ref(false)
+
+</script>
+
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer">
+      <v-list>
+        <v-list-item>
+          <RouterLink to="/">Home</RouterLink>
+        </v-list-item>
+        <v-list-item>
+          <RouterLink to="/about">About</RouterLink>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="() => drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Calendarium</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-app>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <p>Hello World!</p>
+    <v-btn>
+      Button
+    </v-btn>
+  </main>
+</template>
